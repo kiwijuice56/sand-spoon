c29b25f8941ad9d4a473f77cb58ec02c7ed80ca9
kiwijuice56 <alfaroeric127@gmail.com>
2024-07-10 17:45:00 -0500

    Basic documentation, refactoring

---
+++ README.md
@@ -0,0 +1,52 @@
+# chaos-sands
+A modular falling sand simulation created in Godot.
+
+## Elements
+Each type of particle is represented by a Godot `Resource` that
+extends from `Element` (or various subclasses with built-in functionality, such as `Fluid` or `Powder`).
+However, in the simulation itself, particles are represented with only two numbers:
+- `id` a byte representing this particle's element.
+- `data` a 32 bit integer for extra state information. First 16 bits are reserved for storing temperature.
+
+The simulation assigns a unique `id` to each of the `Element` resource when the game starts.
+As the simulation iterates though each cell, it will find the `Element` resource corresponding
+to that particle's `id` and call its `process` and `get_color` functions (which are essentially static).
+By extending the `Element` class, you can also extend the `process` function to interact with the simulation and
+create custom functionality.
+
+### Element functions
+- `func process(sim: Simulation, row: int, col: int, data: int) -> bool:` Advances the particle of this `Element` type (located at `row`, `col` and with state `data`) by one frame.
+When extending this function, it is important to call the parent class's `process` function to preserve its functionality.
+If `process` quits early, it should return `false` if inheriting classes should also quit early.
+
+
+### Sample code
+```python
+MyElement extends Element
+
+func process(sim: Simulation, row: int, col: int, data: int) -> bool:
+    # Always keep this line. If `Element` transforms into
+    # another element and quits early, this element
+    # should not continue processing.
+	if not super.process(sim, row, col, data):
+		return false
+
+    # Temperature is stored in the first 16 bits of `data`
+    # as a 16-bit integer.
+    var current_temperature = get_temperature(data)
+
+    # However, temperature is simulated in different units
+    # internally. Use the `convert_temperature` function
+    # to compare internal temperature data with Kelvin degrees.
+    if current_temperature > convert_temperature(293.0):
+        # Set the 3rd byte with new state information,
+        # then pass into the simulation.
+        data = set_byte(2, 123)
+        sim.set_data(row, col, data)
+    else:
+        sim.set_element(row, col, "sand")
+        return false
+
+    return true
+```
+
--- main/simulation/element/element.gd
+++ main/simulation/element/element.gd
@@ -8,20 +8,29 @@ class_name Element extends Resource
 ## Base color for UI components for this element
 @export var ui_color: Color
 
-@export_group("Performance")
-## The proportion of process frames are skipped. Set to a value higher than 0
-## to hide chunk seams and create more organic movement.
-@export_range(0, 1) var skip_proportion: float = 0.3
-
 @export_group("Heat")
 @export_range(0, 10000, 0.1, "or_greater", "suffix:K") var initial_temperature: float = 293
 @export_range(0, 1) var conductivity: float = 0.5
 @export_subgroup("High")
+## The temperature at which this element will transform into high_heat_transformation. 
+## No effect if set to -1.
 @export_range(-1, 10000, 0.1, "or_greater", "suffix:K") var high_heat_point: float = -1
 @export var high_heat_transformation: String = "empty"
 @export_subgroup("Low")
+## The temperature at which this element will transform into low_heat_transformation. 
+## No effect if set to -1.
 @export_range(-1, 10000, 0.1, "or_greater", "suffix:K") var low_heat_point: float = -1
 @export var low_heat_transformation: String = "empty"
+
+@export_group("Decay")
+## The probability that this element will decay into decay_transformation on any frame.
+@export_range(0, 1) var decay_proportion: float = 0.0
+@export var decay_transformation: String = "empty"
+
+@export_group("Performance")
+## The proportion of process frames are skipped. Set to a value higher than 0
+## to hide chunk seams and create more organic movement.
+@export_range(0, 1) var skip_proportion: float = 0.3
 
 var ihigh_heat_point: int 
 var ilow_heat_point: int 
@@ -33,15 +42,18 @@ func initialize() -> void:
 	if high_heat_point <= 0:
 		ihigh_heat_point = 65535
 	else:
-		ihigh_heat_point = int(sqrt(high_heat_point) * 65535.0/100.0)
+		ihigh_heat_point = convert_temperature(high_heat_point)
 	if low_heat_point <= 0:
 		ilow_heat_point = 0
 	else:
-		ilow_heat_point = int(sqrt(low_heat_point) * 65535.0/100.0)
-	iinitial_temperature = int(sqrt(initial_temperature) * 65535/100.0)
+		ilow_heat_point = convert_temperature(low_heat_point)
+	iinitial_temperature = convert_temperature(initial_temperature)
 
 func process(sim: Simulation, row: int, col: int, data: int) -> bool:
 	if Simulation.fast_randf() < skip_proportion:
+		return false
+	if Simulation.fast_randf() < decay_proportion:
+		sim.set_element(row, col, decay_transformation)
 		return false
 	if Simulation.fast_randf() < conductivity:
 		var new_temp: int = (get_temperature(data) + sim.get_chunk_temp(row, col)) >> 1
@@ -59,6 +71,9 @@ func get_temperature(data: int) -> int:
 
 func set_temperature(data: int, temperature: int) -> int:
 	return (data & 0xFFFF0000) | temperature
+
+func convert_temperature(kelvin_temp: float) -> int:
+	return int(sqrt(kelvin_temp) * 65535.0/100.0)
 
 func get_color(_sim: Simulation, _row: int, _col: int, _data: int) -> Color:
 	return ui_color
